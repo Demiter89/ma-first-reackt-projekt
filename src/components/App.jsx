7bce68fb99c53f3db7f7318a1d2818d8e2f1a533
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-02-27 20:44:02 +0100

    add index file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '/src/assets/react.svg';
 import viteLogo from '/vite.svg'
 import './App.css'
+import Product from './Product'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,4 +33,6 @@ function App() {
   )
 }
 
+
 export default App
+  
